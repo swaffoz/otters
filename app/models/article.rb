92c524a44981f5659281ff0b2f5bad2ffe855b3d
Zane Swafford <zane@otters.io>
2015-12-18 16:58:27 -0600

    Also serialized title

--- app/models/article.rb
+++ app/models/article.rb
@@ -6,6 +6,7 @@ class Article < ActiveRecord::Base
   validates :title, presence: true, uniqueness: true, length: { minimum: 5 }
   validates :text, presence: true
 
+  serialize :title
   serialize :text
   friendly_id :title, use: :slugged
 
