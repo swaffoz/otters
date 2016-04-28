7f5a7087dc6da0e0638a8cab3884ae89115186f5
Zane Swafford <zane@otters.io>
2016-04-27 21:46:28 -0500

    Found out the highlightjs problem with only working on reloads was turbolinks fault. Oh turbolinks, you rascal

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,3 +12,13 @@
 //
 //= require turbolinks
 //= require_tree .
+
+hljs_turbolinks_hack = function() {
+	var elements = document.querySelectorAll('pre code');
+	Array.prototype.forEach.call(elements, function(e, i){
+		hljs.highlightBlock(e)
+	});
+}
+
+document.addEventListener('page:change', hljs_turbolinks_hack, false);
+document.addEventListener('page:restore', hljs_turbolinks_hack, false);
